0540002c971405ad1d8e7575b5bb33d5d4d7aa72
trunkyredstone <romano@lucchesi.co.uk>
2020-10-27 11:46:13 +0000

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -209,7 +209,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    println!("ADAv2 -> V1.1.2");
+    println!("ADAv2 -> V1.1.3");
     println!("ADAv2 -> Initialising");
     let mut settings = Config::default();
     settings.merge(config::File::with_name("Settings")).unwrap();
